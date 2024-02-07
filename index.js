17481d6ab8769dd8af477e6b2bda9b70a06f5d96
mridul <mriduls.webosmotic@gmail.com>
2024-02-07 11:38:26 +0530

    hide table when no data fixed, future dates not allowed fixed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // ############################################# Regrex expressions ###########################################
 
+// restricted future dates selection
+document.forms[0].elements.dob.max = new Date().toISOString().split('T')[0];
+
 const regrexEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{1,5})$/;
 const regrexPhone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
 
@@ -63,7 +66,9 @@ const insertDataToAdvTable = () => {
       let td = document.createElement('td');
       td.classList.add(index);
 
-      td.innerText = val[i] || 'empty';
+      console.log('val', val[i]);
+
+      td.innerText = val[i] === 'null' ? '-' : val[i];
       if (i == 6) {
         td.innerHTML = `  <button onclick="editEntries(${index})">Edit</button>
         <button class="btn" onclick="deleteData(${index})">Delete</button>`;
@@ -120,10 +125,8 @@ const errorHandling = (fieldName) => {
       if (!dob.value && fieldName === 'date') {
         dobFieldLabel.innerText = 'Date of birth is required';
         dobFieldLabel.focus();
+        // console.log('flag2');
 
-        flag.splice(1, 0, false);
-      } else if (new Date(dob.value) >= new Date()) {
-        dobFieldLabel.innerText = 'Future dates are not allowed';
         flag.splice(1, 0, false);
       } else {
         dobFieldLabel.innerText = '';
@@ -157,7 +160,6 @@ const errorHandling = (fieldName) => {
         flag.splice(0, 0, true);
       }
       if (!dob.value) {
-        // console.log('flag2');
         dobFieldLabel.innerText = 'Date of birth is required';
         dobFieldLabel.focus();
 
@@ -346,10 +348,16 @@ const deleteAllBtn = document.querySelector("[value='Delete all']");
 const checkData = () => {
   setData = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : [];
 
+  const mainContainers = document.getElementsByClassName('main-container');
+
   if (setData !== null && setData?.length !== 0) {
+    mainContainers[1].style.display = 'block';
+    mainContainers[2].style.display = 'block';
     deleteAllBtn.style.display = 'block';
   } else {
     deleteAllBtn.style.display = 'none';
+    mainContainers[1].style.display = 'none';
+    mainContainers[2].style.display = 'none';
   }
 
   let previousData = '';
@@ -361,7 +369,7 @@ const checkData = () => {
         <td>${val.dob}</td>
         <td>${val.email}</td>
         <td>${val.phone}</td>
-        <td>${val.hobbies}</td>
+        <td>${val.hobbies === 'null' ? '-' : val.hobbies}</td>
         <td>
           <button onclick="editEntries(${index})">Edit</button>
           <button onclick="deleteData(${index})">Delete</button>
@@ -422,7 +430,7 @@ const searchEntries = (e) => {
         <td>${val.dob}</td>
         <td>${val.email}</td>
         <td>${val.phone}</td>
-        <td>${val.hobbies}</td>
+        <td>${val.hobbies === 'null' ? '-' : val.hobbies}</td>
 
         <td>
           <button onclick="editEntries(${index})">edit</button>
@@ -454,7 +462,7 @@ const sortEntriesAccToDob = () => {
         <td>${val.dob}</td>
         <td>${val.email}</td>
         <td>${val.phone}</td>
-        <td>${val.hobbies}</td>
+        <td>${val.hobbies === 'null' ? '-' : val.hobbies}</td>
 
         <td>
           <button onclick="editEntries(${index})">Edit</button>
@@ -479,7 +487,3 @@ const deleteAll = () => {
     insertDataToAdvTable();
   }
 };
-
-// const datePicker = document.querySelector('[type="date"]');
-
-// datePicker.setAttribute('max', new Date());
